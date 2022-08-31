d5264ba1daaf509e5bdccfd6960195495eec95ee
Павел <58857483+Haskiro@users.noreply.github.com>
2022-08-31 21:25:17 +0300

    layout created, footer create

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,62 @@
-import { Htag, Button } from '../components';
+import { GetStaticProps } from 'next';
+import { useEffect, useState } from 'react';
+import { Htag, Button, P, Tag, Rating } from '../components';
+import { withLayout } from '../layout/Layout';
+import axios from 'axios';
+import { MenuItem } from '../interfaces/menu.interface';
 
-export default function Home(): JSX.Element {
+function Home({ menu }: HomeProps): JSX.Element {
+  const [counter, setCounter] = useState<number>(0);
+  const [rating, setRating] = useState<number>(4);
+
+  useEffect(() => {
+    console.log('Counter ' + counter);
+    return function cleanup() {
+      console.log('Unmount');
+    };
+  });
+
+  useEffect(() => {
+    console.log('Mounted');
+  });
+
+
   return (
-    <div>
+    <>
       Ассалам алейкум
-      <Htag tag="h3">Текст</Htag>
-      <Button appearence='primary' arrow='down'>Кнопка</Button>
+      <Htag tag="h1">{counter}</Htag>
+      <Button appearence='primary' arrow='down' onClick={() => setCounter(x => x + 1)}>Кнопка</Button>
       <Button appearence='ghost' arrow='right'>Кнопка</Button>
-    </div>
+      <P size='l'>Большой</P>
+      <P>Средний</P>
+      <Tag>Ghost</Tag>
+      <Tag color='red' href='https://vk.com'>Red</Tag>
+      <Tag size='m' color='green'>Green</Tag>
+      <Tag size='m' color='primary'>Primary</Tag>
+      <Rating rating={rating} isEditable={true} setRating={setRating} />
+      <ul>
+        {menu.map(m => (<li key={m._id.secondCategory}>{m._id.secondCategory}</li>))}
+      </ul>
+    </>
   );
+}
+
+export default withLayout(Home);
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+  const firstCategory = 0;
+  const { data: menu } = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', {
+    firstCategory
+  });
+  return {
+    props: {
+      menu,
+      firstCategory
+    }
+  };
+};
+
+interface HomeProps extends Record<string, unknown> {
+  menu: MenuItem[],
+  firstCategory: number,
 }
